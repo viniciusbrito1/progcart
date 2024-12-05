f827faf3ab2deddae500f5eb0686318fbfe3ade6
mauriciodev <mauricio.dev@gmail.com>
2024-12-05 11:21:48 -0300

    Adicionando exemplo de código no modelo.

---
+++ projeto_modelo/src/projeto_modelo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python # [1]
+"""\
+Descrição deste arquivo
+
+Usage: myscript.py BAR1 BAR2
+"""
+
+# Bibliotecas do Python
+import os
+
+# Bibliotecas não nativas do python
+
+# Bibliotecas deste projeto (outros pedaços de codigo)
+
+print("Hello")
+
+# Principal metodo
+def main():
+    print("hey there")
+
+# Se este script for executado individualmente, vai executar esta parte
+if __name__=="__main__":
+    main()
+
