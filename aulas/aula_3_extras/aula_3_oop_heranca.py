729e9872a834a13e45074f9ec83be892062f009a
mauriciodev <mauricio.dev@gmail.com>
2025-02-18 08:23:11 -0300

    Exemplos

--- aulas/aula_3_extras/aula_3_oop_heranca.py
+++ aulas/aula_3_extras/aula_3_oop_heranca.py
@@ -1,27 +1,26 @@
 import numpy as np
+import abc
 
+class geometria:
+    def __init__(self,lista_de_lista):
+        self.coordenadas = np.array(lista_de_lista)
+    
+    @abc.abstractmethod
+    def comprimento():
+        pass
 
-class ponto:
-    def __init__(self,x,y):
-        self.coordenadas = np.array([[x,y]])
-
+class ponto (geometria):
     def comprimento(self):
         return 0
 
-class linha:
-    def __init__(self, lista_de_lista):
-        self.coordenadas = np.array(lista_de_lista)
-
+class linha (geometria):
     def comprimento(self):
         comprimento_parcial = 0
         for i in range(1, len(self.coordenadas)):
             comprimento_parcial += np.linalg.norm(self.coordenadas[i] - self.coordenadas[i-1])
         return comprimento_parcial
 
-class poligono:
-    def __init__(self, lista_de_lista):
-        self.coordenadas = np.array(lista_de_lista)
-      
+class poligono (geometria):
     def comprimento(self):
         comprimento_parcial = 0
         for i in range(1, len(self.coordenadas)):
@@ -29,7 +28,7 @@ class poligono:
         return comprimento_parcial
 
 if __name__ == "__main__":
-    ponto_obj = ponto(0,0)
+    ponto_obj = ponto([[0,0]])
     linha_obj = linha([[0,0], [1,0], [1,1]])
     poligono_obj = poligono([[0,0], [1,0], [1,1], [0,1]])
 
